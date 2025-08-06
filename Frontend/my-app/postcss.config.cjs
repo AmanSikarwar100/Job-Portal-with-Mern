eb9e8a39eab0cf8b99465ec1be77fe02a7ed461d
Aman Sikarwar <thakursahabasgs100@gmail.com>
2025-08-06 21:59:13 +0530

    Frontend Push

---
+++ Frontend/my-app/postcss.config.cjs
@@ -0,0 +1,26 @@
+// module.exports = {
+//   plugins: {
+//     tailwindcss: {},
+//     autoprefixer: {},
+//   },
+// }
+// export default {
+//   plugins: {
+//     tailwindcss: {},
+//     autoprefixer: {},
+//   },
+// };
+// export default {
+//   plugins: {
+//     '@tailwindcss/postcss': {},
+//     autoprefixer: {},
+//   },
+// };
+module.exports = {
+  plugins: {
+    '@tailwindcss/postcss': {},
+    autoprefixer: {},
+  },
+}
+
+
